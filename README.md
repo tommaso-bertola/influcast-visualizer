8d43c554b7e5f2a5fe172ea962ac8f516d6e78bc
Tommaso Bertola <bertola.tommaso@hotmail.it>
2025-03-18 10:01:54 +0100

    add virus strain comparison

--- README.md
+++ README.md
@@ -10,3 +10,23 @@ Competing teams are displayed on the right.
 
 This is a comparison of the past influenza seasons.
 ![Maxima](./previsioni/plot_maxima.png)
+
+This elaborates the data from [Epicentro ISS](https://www.epicentro.iss.it/influenza/respinews#vir)
+using the following aggregations
+
+| ISS                        | Conversion | Conversion |
+| :------------------------- | ---------- | ---------- |
+| A/non sottotipizzati       | A          | A          |
+| A(H1N1)                    | A          | A          |
+| A(H3N2)                    | A          | A          |
+| B                          | B          | A          |
+| Virus Parainfluenzali      | Other      | A          |
+| SARS-CoV2                  | Other      | Other      |
+| Adenovirus                 | Other      | Other      |
+| Bocavirus                  | Other      | Other      |
+| Coronavirus (no SARS CoV2) | Other      | Other      |
+| Metapneumovirus            | Other      | Other      |
+| Rhinovirus                 | Other      | Other      |
+| RSV                        | Other      | Other      |
+
+![Strains](./compare_a_b_iss_influcast.jpg)
--- sorveglianza/compare_a_b_iss_influcast.R
+++ sorveglianza/compare_a_b_iss_influcast.R
@@ -1,9 +1,11 @@
 # Load necessary library
 library(readr)
 library(ggplot2)
+library(ggpubr)
 library(reshape2)
 library(magrittr)
 library(dplyr)
+library(tidyr)
 # Load the CSV file
 andamento_settimanale_de <- read_csv("/Users/tommasobertola/Git/influcast-local-plots/sorveglianza/andamento-settimanale-de.csv")
 
@@ -12,7 +14,8 @@ head(andamento_settimanale_de)
 
 melted_andamento <- andamento_settimanale_de %>%
     select(-`Andamento RespiVirNet-Epi`) %>%
-    melt(, id.vars = c("Settimana"))
+    melt(, id.vars = c("Settimana")) %>%
+    mutate(Settimana = factor(Settimana, levels = unique(Settimana)))
 
 
 influ_a <- "Influ A"
@@ -33,24 +36,47 @@ list_pathogens <- c(
     "Virus Parainfluenzali" = influ_others
 )
 
+list_pathogens_2 <- c(
+    "A/non sottotipizzati" = influ_a,
+    "A(H1N1)" = influ_a,
+    "A(H3N2)" = influ_a,
+    "B" = influ_a,
+    "SARS-CoV2" = influ_others,
+    "Adenovirus" = influ_others,
+    "Bocavirus" = influ_others,
+    "Coronavirus (no SARS CoV2)" = influ_others,
+    "Metapneumovirus" = influ_others,
+    "Rhinovirus" = influ_others,
+    "RSV" = influ_others,
+    "Virus Parainfluenzali" = influ_a
+)
+
 df <- data.frame(
     Name = names(list_pathogens), # Extract names
     Value = unlist(list_pathogens, use.names = FALSE) # Extract values
 )
 
+df_2 <- data.frame(
+    Name = names(list_pathogens_2), # Extract names
+    Value_2 = unlist(list_pathogens_2, use.names = FALSE) # Extract values
+)
+
 
 pathogens <- melted_andamento %>%
-    left_join(df, by = c("variable" = "Name"))
+    left_join(df, by = c("variable" = "Name")) %>%
+    left_join(df_2, by = c("variable" = "Name"))
 
 pathogens %>%
     group_by(Settimana) %>%
     mutate(perc = value / sum(value) * 100) %>%
     ggplot(aes(x = Settimana, y = value, fill = variable)) +
     geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
-    geom_text(aes(label = paste0(round(perc, 0),'%')), position = position_dodge(width = 0.9), vjust = -0.5)
+    geom_text(aes(label = paste0(round(perc, 0), "%")),
+        position = position_dodge(width = 0.9), vjust = -0.5, size = 3
+    )
 
 
-pathogens %>%
+p1 <- pathogens %>%
     group_by(Settimana, Value) %>%
     summarise(sum = sum(value)) %>%
     ungroup() %>%
@@ -58,4 +84,36 @@ pathogens %>%
     mutate(perc = sum / sum(sum) * 100) %>%
     ggplot(aes(x = Settimana, y = perc, fill = Value)) +
     geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
-    geom_text(aes(label = round(perc, 1)), position = position_dodge(width = 0.9), vjust = -0.5)
+    geom_text(aes(label = round(perc, 1)), position = position_dodge(width = 0.9), vjust = -0.5, size = 3) +
+    labs(x = "Week", y = "Percentage", fill = "") +
+    theme(legend.position = "top")
+
+
+p2 <- pathogens %>%
+    group_by(Settimana, Value_2) %>%
+    summarise(sum = sum(value)) %>%
+    ungroup() %>%
+    group_by(Settimana) %>%
+    mutate(perc = sum / sum(sum) * 100) %>%
+    ggplot(aes(x = Settimana, y = perc, fill = Value_2)) +
+    geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
+    geom_text(aes(label = round(perc, 1)),
+        position = position_dodge(width = 0.9),
+        vjust = -0.5, check_overlap = F, size = 3
+    ) +
+    labs(x = "Week", y = "Percentage", fill = "") +
+    theme(legend.position = "top")
+
+p <- ggarrange(p1, p2, ncol = 1)
+annotate_figure(p, top = text_grob("ISS strain prevalence for 2024-2025 season", face = "bold", size = 14))
+ggsave("compare_a_b_iss_influcast.jpg", width = 8, height = 10)
+
+pathogens %>%
+    group_by(Settimana, Value_2) %>%
+    summarise(sum = sum(value)) %>%
+    ungroup() %>%
+    group_by(Settimana) %>%
+    mutate(perc = sum / sum(sum) * 100) %>%
+    select(-sum) %>%
+    pivot_wider(names_from = Value_2, values_from = perc) %>%
+    write_csv("compare_a_b_iss_influcast.csv")
